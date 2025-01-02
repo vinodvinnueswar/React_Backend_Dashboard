7737c7cc5c0aa0a2f0e1677c5498963c811aba4c
Sasispecious <sasispecious@gmail.com>
2025-01-02 08:29:45 -0800

    changed api path

--- src/helpers/ApiPath.js
+++ src/helpers/ApiPath.js
@@ -1,4 +1,4 @@
 
 // export const API_URI = 'http://localhost:4000'
 
-export const API_URI = 'https://backend-nodejs-tzpn.onrender.com'
+export const API_URI = "https://backend-nodejs-tzpn.onrender.com"
